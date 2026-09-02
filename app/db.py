38278ff6bceb92a1f9d5a9c3d92d2cef5f63e00a
Scott <bndrbots@gmail.com>
2026-09-02 00:08:54 -0700

    Create db.py

---
+++ app/db.py
@@ -0,0 +1,17 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
+
+from app.config import get_settings
+
+settings = get_settings()
+
+engine = create_engine(
+    settings.database_url,
+    pool_pre_ping=True,
+    future=True,
+)
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False, future=True)
+
+
+class Base(DeclarativeBase):
+    """Declarative base for all ORM models."""
